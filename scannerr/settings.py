acadd8f08cf70984f5755de20350c54b6b836c58
fyung36 <36994142+fyung36@users.noreply.github.com>
2020-02-27 10:21:49 +0100

    morning day 3

--- scannerr/settings.py
+++ scannerr/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR, 'pictureURL/static', 'airtel/static').replace('\\', '/')
+STATIC_DIR = os.path.join(BASE_DIR,'airtel/static').replace('\\', '/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -25,7 +25,7 @@ SECRET_KEY = '3+drmpm!id+u9b*ele%vak$h%-jpbem=e5l6^e=v$-ad(&%(p%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.24.213.131','127.0.0.1', 'novacampaignapp.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'novacampaignapp.herokuapp.com']
 
 LOGIN_URL = "/login"
 
